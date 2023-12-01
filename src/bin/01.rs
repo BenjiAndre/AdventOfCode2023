ef2a8f422356aa2ad15d6b08547a679696b0d550
Benjamin Andre <benjaminandre23@gmail.com>
2023-12-01 09:35:38 +0100

    updated crates

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,13 +1,15 @@
 const STRING_TO_DIGIT: &[&[u8]] = &[
-    b"zero", b"one", b"two", b"three", b"four", b"five", b"six", b"seven", b"eight", b"nine"
+    b"zero", b"one", b"two", b"three", b"four", b"five", b"six", b"seven", b"eight", b"nine",
 ];
 
 fn digit_sum(line: &[u8], spelled: bool) -> usize {
     let mut digits = line.iter().enumerate().filter_map(|(i, &c)| {
-        if c.is_ascii_digit() { 
+        if c.is_ascii_digit() {
             Some((c - b'0') as usize)
         } else if spelled {
-            STRING_TO_DIGIT.iter().position(|&d| line[i..].starts_with(d))
+            STRING_TO_DIGIT
+                .iter()
+                .position(|&d| line[i..].starts_with(d))
         } else {
             None
         }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,11 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, AttributeArgs, ItemFn, NestedMeta, Lit, Ident};
+use syn::{parse_macro_input, Ident, ItemFn, LitInt};
 
 #[proc_macro_attribute]
 pub fn main(args: TokenStream, input: TokenStream) -> TokenStream {
-    let input_path = match &parse_macro_input!(args as AttributeArgs)[..] {
-        [NestedMeta::Lit(Lit::Int(day))] => format!("../../inputs/{}.in", day.token()),
-        _ => panic!("Expected one integer argument"),
-    };
+    let day = parse_macro_input!(args as LitInt);
+    let input_path = format!("../../inputs/{}.in", day.token());
 
     let mut aoc_solution = parse_macro_input!(input as ItemFn);
     aoc_solution.sig.ident = Ident::new("aoc_solution", aoc_solution.sig.ident.span());
@@ -19,8 +17,8 @@ pub fn main(args: TokenStream, input: TokenStream) -> TokenStream {
             let now = std::time::Instant::now();
             let (p1, p2) = aoc_solution(INPUT.trim_end());
             let elapsed = now.elapsed();
-            println!("Part one: {}", p1);
-            println!("Part two: {}", p2);
+            println!("Part 1: {}", p1);
+            println!("Part 2: {}", p2);
             if elapsed.as_millis() > 0 {
                 println!("Time: {}ms", elapsed.as_millis());
             } else {
@@ -33,10 +31,8 @@ pub fn main(args: TokenStream, input: TokenStream) -> TokenStream {
 
 #[proc_macro_attribute]
 pub fn test_main(args: TokenStream, input: TokenStream) -> TokenStream {
-    let input_path = match &parse_macro_input!(args as AttributeArgs)[..] {
-        [NestedMeta::Lit(Lit::Int(day))] => format!("../../test_inputs/{}.in", day.token()),
-        _ => panic!("Expected one integer argument"),
-    };
+    let day = parse_macro_input!(args as LitInt);
+    let input_path = format!("../../test_inputs/{}.in", day.token());
 
     let mut aoc_solution = parse_macro_input!(input as ItemFn);
     aoc_solution.sig.ident = Ident::new("aoc_solution", aoc_solution.sig.ident.span());
@@ -48,8 +44,8 @@ pub fn test_main(args: TokenStream, input: TokenStream) -> TokenStream {
             let now = std::time::Instant::now();
             let (p1, p2) = aoc_solution(INPUT.trim_end());
             let elapsed = now.elapsed();
-            println!("Part one: {}", p1);
-            println!("Part two: {}", p2);
+            println!("Part 1: {}", p1);
+            println!("Part 2: {}", p2);
             if elapsed.as_millis() > 0 {
                 println!("Time: {}ms", elapsed.as_millis());
             } else {
